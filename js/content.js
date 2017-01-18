90446005cb41523190ba85f866ba603f96bbbed0
Dan Hall <danny@Dominiks-Mac-mini.local>
2017-01-18 15:32:51 +0100

    intial commit

---
+++ js/content.js
@@ -0,0 +1,16 @@
+//slider for home page//
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 4 seconds//
+}
+//slider for home page//
