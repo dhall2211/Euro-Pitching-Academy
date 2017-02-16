89a22da60949e556da9700b16c75de7ad8a17d3c
dhall2211 <dhall2211@gmail.com>
2017-02-16 15:24:43 +0100

    js hide/showmobile nav
    small nav can be accessed with a click

--- js/content.js
+++ js/content.js
@@ -1,5 +1,5 @@
 //login button//
-<script>
+
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -9,21 +9,20 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-</script>
 
-//slider for home page//
-var myIndex = 0;
-carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
+
+
+function showHideMobileNavigation() {
+    var smalldpdn = document.getElementById("respon-navbar")
+
+
+    if (smalldpdn.style.display === 'none') {
+        //open
+        smalldpdn.style.display = 'flex';
+
+    } else {
+        //close
+        smalldpdn.style.display = 'none';
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 4000); // Change image every 4 seconds//
 }
-//slider for home page//
