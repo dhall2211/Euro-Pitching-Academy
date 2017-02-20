06ff9aec16649c53768e117cf4f762d5c7c5742f
dhall2211 <dhall2211@gmail.com>
2017-02-20 15:36:33 +0100

    sign in sign up buttons
    the sign in and sign up buttons are now integrated with the footer and
    are responsive.

--- js/content.js
+++ js/content.js
@@ -1,5 +1,4 @@
-//login button//
-
+//signup
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -11,9 +10,20 @@ window.onclick = function(event) {
 }
 
 
+//login button
+
+// Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
-function showHideMobileNavigation() {
+function openclosedropdn() {
     var smalldpdn = document.getElementById("respon-navbar")
 
 
@@ -25,4 +35,17 @@ function showHideMobileNavigation() {
         //close
         smalldpdn.style.display = 'none';
     }
+}
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
 }
